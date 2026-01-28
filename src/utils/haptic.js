cd36646dec211d4e228c2f01e6d403b748847dc7
Razex4777 <Razex4777@users.noreply.github.com>
2026-01-28 16:00:08 +0100

    fix: Relocate git repo to part2 folder

--- src/utils/haptic.js
+++
@@ -1,9 +0,0 @@
-/**
- * Trigger device haptic feedback
- * @param {number} duration - Vibration duration in ms
- */
-export const triggerHaptic = (duration = 15) => {
-    if (typeof navigator !== 'undefined' && navigator.vibrate) {
-        navigator.vibrate(duration);
-    }
-};
